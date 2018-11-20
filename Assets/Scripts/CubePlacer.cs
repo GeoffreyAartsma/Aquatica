dc41d167da7d2fc6a777f88742da6c4bade50218
Geoffrey <GeoffreyAartsma@outlook.com>
2018-11-20 22:54:11 +0100

    Initial commit

---
+++ Assets/Scripts/CubePlacer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePlacer : MonoBehaviour {
+
+    [SerializeField]
+    private GridManager grid;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            { 
+                PlaceCubeNear(hitInfo.point);
+            }
+        }
+    }
+
+    private void PlaceCubeNear(Vector3 clickPoint)
+    {
+        Debug.Log("PCN");
+        Vector3 finalposition = grid.GetNearestPointOnGrid(Vector3.down);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalposition;
+        //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = nearPoint;
+    }
+}
