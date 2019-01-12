df0ba7ccef7baefd81ba7bbd178ab9743c455ef9
[geoffreyaartsma@outlook.com] <GeoffreyAartsma@outlook.com>
2019-01-12 21:05:09 +0100

    Healthbar, health buildings, health enemys

---
+++ Assets/Scripts/BuildingHealth.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BuildingHealth : MonoBehaviour
+{
+    public float startHealth = 100;
+    private float currentHealth;
+
+    public Image healthBar;
+
+    bool isDead;
+
+    void Start()
+    {
+        currentHealth = startHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+
+        healthBar.fillAmount = currentHealth / startHealth;
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Dead();
+        }
+    }
+
+    private void Dead()
+    {
+        isDead = true;
+    }
+
+}
+
