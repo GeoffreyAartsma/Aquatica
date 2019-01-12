78cddb16200594c7ac88fda0a315d0394a097754
danny <dannydorstijn1997@gmail.com>
2019-01-13 00:01:43 +0100

    Started developing wave system

---
+++ Assets/Scripts/WaveTimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveTimer : MonoBehaviour
+{
+    float progress;
+    void Start()
+    {
+        progress = 0f;
+    }
+
+    void Update()
+    {
+        progress += 1f * Time.deltaTime;
+
+        if (progress > 100f)
+        {
+            SpawnWave();
+        }
+    }
+
+    void SpawnWave()
+    {
+
+    }
+
+    public void Reset()
+    {
+        progress = 0f;
+    }
+}
