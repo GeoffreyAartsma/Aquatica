bf5737296d55446a26a99384244fc64592fcabd8
GeoffreyAartsma <GeoffreyAartsma@outlook.com>
2019-01-16 23:34:04 +0100

    begin camera script

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,15 +5,66 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
+    [SerializeField]
+    GridManager grid;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("grid width: " + grid.GridWidth);
+            SpawnEnemy();
+        }
+    }
 
+    void SpawnEnemy(bool retry = false)
+    {
+        Vector2Int coord = Vector2Int.zero;
+
+        switch (Random.Range(0, 4))
+        {
+            case 0: // North
+                Debug.Log("NORTH");
+                coord = new Vector2Int(Random.Range(0, grid.GridWidth - 1), grid.GridWidth - 1);
+                break;
+            case 1: // East
+                coord = new Vector2Int(grid.GridWidth - 1, Random.Range(0, grid.GridWidth - 1));
+                Debug.Log("EAST");
+                break;
+            case 2: // South
+                coord = new Vector2Int(Random.Range(0, grid.GridWidth - 1), 0);
+                Debug.Log("SOUTH");
+                break;
+            case 3: // West
+                coord = new Vector2Int(0, Random.Range(0, grid.GridWidth - 1));
+                Debug.Log("WEST");
+                break;
+
+            default:
+                break;
+        }
+
+        if (!grid.IsOccupied[coord.x, coord.y])
+        {
+            // Cube wordt gespawnd
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            // Positie van cube wordt bepaald door de coordinate die hierboven wordt aangegeven
+            cube.transform.position = grid.GetPositionFromCoordinate(coord);
+            // Cube wordt blauw gemaakt
+            cube.GetComponent<MeshRenderer>().material.color = Color.blue;
+            // Coordinate wordt bezet
+            grid.IsOccupied[coord.x, coord.y] = true;
+        }
+        else
+        {
+            if (retry)
+            {
+                Debug.LogError("All spaces are occupied!");
+                return;
+            }
+            SpawnEnemy(true);
+        }
     }
 }
+
---
+++ Assets/Scripts/cameraMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour
+{
+
+    public int Boundary = 50;
+    public int speed = 5;
+
+    private int screenWidth;
+    private int screenHeight;
+
+
+    void Start()
+    {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+    }
+
+
+    void Update()
+    {
+
+    }
+}
+
